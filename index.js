06210295fc2a2f0c5c3dc2004ea41128bdb7bffe
Aaron Levy <aaronlevypc21@gmail.com>
2023-12-10 00:12:44 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-// Code your solution in this file!
-const companyName = 'Scuber';
-let mostProfitableNeighborhood = 'Chelsea'
-let companyCeo ='Susan Smith'
+let cmopanyName = "Scuber";
+const companyName = "Scuber";
+
+let mostProfitableNeighborhood = "Chelsea";
+
+let companyCeo = "Susan Smith";
